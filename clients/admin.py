055821712eb037014d4bbab8136d810f5a94d373
adrianapalushi <adrianapalushi@gmail.com>
2026-04-20 12:51:00 +0200

    Add client model and client list/create API

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email', 'company', 'status', 'created_at']
+    search_fields = ['name', 'email', 'company']
+    list_filter = ['status', 'created_at']
+    
--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Client
+from .serializers import ClientSerializer
 
-# Create your views here.
+
+class ClientListCreateView(generics.ListCreateAPIView):
+    serializer_class = ClientSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Client.objects.filter(user=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
